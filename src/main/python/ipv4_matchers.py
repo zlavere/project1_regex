6d23fff82c1b35689ebdd1e0bab7df21a61397c7
Lewis Baumstark <lewisb@westga.edu>
2019-09-14 13:40:26 -0400

    init

---
+++ src/main/python/ipv4_matchers.py
@@ -0,0 +1,11 @@
+import re
+
+# todo: complete this regex
+ipv4Regex = '\b'
+
+def isIpv4Address(address):
+    # todo: complete
+    return False
+
+# todo: complete this regex
+netmaskRegex = None
---
+++ src/unittest/python/is_ipv4_address_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from ipv4_matchers import *
+
+# sample test class
+class TestIsIPv4Address(unittest.TestCase):
+
+    # sample test method
+    def testThisTestWillFail(self):
+        expected = 5
+        self.assertTrue(True)
+        self.assertEqual(1, expected)
