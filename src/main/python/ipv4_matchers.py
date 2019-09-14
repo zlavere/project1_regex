fa7c912e83356ab0d2fffe01c704469b522115a3
Lewis Baumstark <lewisb@westga.edu>
2019-09-14 17:14:01 -0400

    is IPv4Address written and tested

--- src/main/python/ipv4_matchers.py
+++ src/main/python/ipv4_matchers.py
@@ -1,10 +1,18 @@
 import re
 
 # todo: complete this regex
-ipv4Regex = '\b'
+ipv4Regex = r'^\b([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\b$'
 
 def isIpv4Address(address):
-    # todo: complete
+    if address == None:
+        return False
+
+    regex = re.compile(ipv4Regex)
+    searchResult = regex.search(address)
+
+    if searchResult != None:
+        return True
+
     return False
 
 # todo: complete this regex
--- src/unittest/python/is_ipv4_address_tests.py
+++ src/unittest/python/is_ipv4_address_tests.py
@@ -4,8 +4,41 @@ from ipv4_matchers import *
 # sample test class
 class TestIsIPv4Address(unittest.TestCase):
 
-    # sample test method
-    def testThisTestWillFail(self):
-        expected = 5
-        self.assertTrue(True)
-        self.assertEqual(1, expected)
+    def testPassValidIPv4Address(self):
+        self.assertTrue(isIpv4Address('160.010.025.006'))
+
+    def testInvalidAllOctetsTooHighIPv4Address(self):
+        self.assertFalse(isIpv4Address('988.988.988.988'))
+
+    def testInvalidNoneAsAddressParameterIPv4Address(self):
+        self.assertFalse(isIpv4Address(None))
+
+    def testInvalidTooShortOneMissingOctetIPv4Address(self):
+        self.assertFalse(isIpv4Address('168.168.168'))
+
+    def testValidAllTwoDigitOctetsIPv4Address(self):
+        self.assertTrue(isIpv4Address('12.12.12.12'))
+
+    def testValidAllLeadingZeroTwoDigitOctetsIPv4Address(self):
+        self.assertTrue(isIpv4Address('01.01.01.01'))
+
+    def testValidTwoLeadingZeroOctetsIPv4Address(self):
+        self.assertTrue('001.001.001.001')
+
+    def testSingleNonZeroDigitOctetIPv4Address(self):
+        self.assertTrue('1.1.1.1')
+
+    def testInvalidTooLongOneExtraOctetIPv4Address(self):
+        self.assertFalse(isIpv4Address('168.168.168.168.168'))
+
+    def testValidAllTwoFiveFiveOctetIPv4Address(self):
+        self.assertTrue(isIpv4Address('255.255.255.255'))
+
+    def testValidAllZeroOctetIPv4Address(self):
+        self.assertTrue(isIpv4Address('0.0.0.0'))
+
+    def testInvalidOneOctetTooLongIPv4Address(self):
+        self.assertFalse(isIpv4Address('123.123.1234.123'))
+
+    def testInvalidOneOctetHasNoValueIPv4Address(self):
+        self.assertFalse(isIpv4Address('123.123..123'))
