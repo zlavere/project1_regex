f94464fb6a70a98a413fb24ffcec182a8708e7d0
Lewis Baumstark <lewisb@westga.edu>
2019-09-16 13:50:36 -0400

    split regex into verbose mode

--- src/main/python/ipv4_matchers.py
+++ src/main/python/ipv4_matchers.py
@@ -1,14 +1,27 @@
 import re
 
-# todo: complete this regex
-ipv4Regex = r'(^|(?=\s))([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)($|\s)'
+ipv4Regex = re.compile(r'''(
+(^|(?=\s))                                  # start anchors
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.   # first Octet
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.   # second Octet
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.   # third Octet
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)     # last Octet
+($|\s)                                      # end anchors
+)''',re.VERBOSE | re.MULTILINE)
+
 
 def isIpv4Address(address):
+    """Determines if a string is a valid IPv4 address
+
+    Args:
+        address: String to validate as IPv4 address
+    Returns:
+        True if address is a valid IPv4 address, otherwise False
+    """
     if address == None:
         return False
 
-    regex = re.compile(ipv4Regex)
-    searchResult = regex.search(address)
+    searchResult = ipv4Regex.search(address)
 
     if searchResult != None:
         return True
@@ -16,18 +29,32 @@ def isIpv4Address(address):
     return False
 
 def extractIpv4AddressesFrom(text):
+    """Extracts valid IPv4 addresses from argument string.
+
+    Args:
+        text: Text from which all valid IPv4 Addresses will be extracted.
+    Returns:
+        A list of all extracted IPv4 Addresses.
+    """
     if text == None:
         return None
 
-    regex = re.compile(ipv4Regex, re.MULTILINE)
-    addresses = regex.finditer(str(text))
+    addresses = ipv4Regex.finditer(str(text))
     results = []
 
     for current in addresses:
         results.append(current.group())
 
+    if not results:
+        results = None
+
     return results
 
-
-# todo: complete this regex
-netmaskRegex = None
+netmaskRegex = re.compile(r'''(
+(^|(?=\s))                                  # start anchors
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.   # first Octet
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.   # second Octet
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)\.   # third Octet
+([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]?)     # last Octet
+($|\s)                                      # end anchors
+)''',re.VERBOSE | re.MULTILINE)
